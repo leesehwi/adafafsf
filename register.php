a39a6b7815979e380aa3b4a82e1f84f729689566
leesehwi <76462814+leesehwi@users.noreply.github.com>
2021-01-25 20:40:53 +0900

    Add files via upload

---
+++ register.php
@@ -0,0 +1,27 @@
+<?php
+$userid = $_POST['userid'];
+$password = $_POST['password'];
+$username = $_POST['username'];
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+$dept = $_POST['dept'];
+
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "gs9597";
+$dbname = "sehwiweb";
+// Create connection
+
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+if (mysqli_connect_error()){
+    die('Connect Error ('. mysqli_connect_errno() .') '
+    . mysqli_connect_error());
+}
+else{
+$sql = "INSERT INTO account (userid, password, username, gender, email, dept)
+values ('$userid','$password', '$username', '$gender', '$email', '$dept')";
+    if ($conn->query($sql)){
+        echo "<script>location.href='success.html'</script>";
+    }
+}
+?>
